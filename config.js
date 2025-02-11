1955e7032b08d79a1e8bc3432fb7e2006295ac52
uhppham <nyphamphu@gmail.com>
2025-02-10 20:03:25 -0500

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dg5xp1iaj/video/upload/v1739235618/hmdaam1o89zzp6p3eg5s.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
