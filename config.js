0fd7de3a3156e44263994f36788ee2a1dc8bf2b5
uhppham <nyphamphu@gmail.com>
2025-02-10 21:31:34 -0500

    Update config.js

--- config.js
+++ config.js
@@ -50,7 +50,7 @@ const CONFIG = {
     // Messages that appear after they say "Yes!"
     celebration: {
         title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
+        message: "Now come and give cuddles, and we go on nice date! AYYAYAYAYYAYA!!", 
         emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
     },
 
